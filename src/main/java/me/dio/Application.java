444e987ecbec93bf2e709417a4d2682b4e18574c
neildojr <neildo.donascimentojr@gmail.com>
2023-10-12 17:49:33 -0300

    Alterando o nome da classe de aplicação

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SantandeDevWeek2023Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SantandeDevWeek2023Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
