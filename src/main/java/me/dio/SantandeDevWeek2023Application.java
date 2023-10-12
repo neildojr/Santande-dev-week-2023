cac0bbb798429ac509473f8de424c663ff055ed0
neildojr <neildo.donascimentojr@gmail.com>
2023-10-12 07:27:03 -0300

    Initial commit

---
+++ src/main/java/me/dio/SantandeDevWeek2023Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantandeDevWeek2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantandeDevWeek2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/SantandeDevWeek2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantandeDevWeek2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
