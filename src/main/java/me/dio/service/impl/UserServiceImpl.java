f1a3ca0f808697b39c7db44bd856bb916fc7f14f
neildojr <neildo.donascimentojr@gmail.com>
2023-10-17 14:20:45 -0300

    Criando UserService

--- src/main/java/me/dio/service/impl/UserServiceImpl.java
+++ src/main/java/me/dio/service/impl/UserServiceImpl.java
@@ -1,20 +1,21 @@
-package me.dio.domain.service.impi;
+package me.dio.service.impl;
 
 import me.dio.domain.model.User;
 import me.dio.domain.repository.UserRepository;
-import me.dio.domain.service.UserService;
+import me.dio.service.UserService;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
 
 @Service
-public class UserServiceImpi implements UserService {
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
-    public UserServiceImpi(UserRepository userRepository){
+    public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
     @Override
     public User findById(Long id) {
         return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
@@ -22,8 +23,8 @@ public class UserServiceImpi implements UserService {
 
     @Override
     public User create(User userToCreate) {
-        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())){
-            throw  new IllegalArgumentException("This Acoount number already exists.");
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+            throw new IllegalArgumentException("This Account number already exists.");
         }
         return userRepository.save(userToCreate);
     }
